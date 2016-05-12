648e251156de6ce3f07a3a85c9cd6f998548ce70
piotrromanowski <piotrromanowski1@gmail.com>
2016-05-11 22:22:38 -0400

    Append initial commit

--- src/app/ask-me-anything.component.ts
+++ src/app/ask-me-anything.component.ts
@@ -1,8 +1,48 @@
 import { Component } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { HomeComponent } from './+home';
-import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS, Router} from '@angular/router';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS, Router, } from '@angular/router';
 import { AboutComponent } from './+about';
+
+// @Component({
+//   moduleId: module.id,
+//   selector: 'ask-me-anything-app-child',
+//   templateUrl: 'ask-me-anything.component.html',
+//   styleUrls: ['ask-me-anything.component.css'],
+//   directives: [ROUTER_DIRECTIVES],
+//   providers: [ROUTER_PROVIDERS]
+// })
+// @Routes([
+//   {path: '/', component: HomeComponent},
+//   {path: '/about', component: AboutComponent}
+// ])
+// export class AskMeAnythingAppComponentChild {
+//   constructor(private router:Router) {
+//   }
+  
+//   isActive(route) {
+//     return location.pathname === route;
+//   }  
+// }
+
+
+// @Component({
+//   selector: 'ask-me-anything-app',
+//   directives: [ROUTER_DIRECTIVES],
+//   providers: [ROUTER_PROVIDERS],
+//   template: `
+//     <div>
+//     HI
+//         <router-outlet></router-outlet>
+//     </div>
+//   `
+// })
+// @Routes([
+//   {path: '/ask-me-anything/..', component: AskMeAnythingAppComponentChild }
+// ])
+// export class AskMeAnythingAppComponent {
+//     constructor() {}
+// }
 
 @Component({
   moduleId: module.id,
@@ -13,8 +53,8 @@ import { AboutComponent } from './+about';
   providers: [ROUTER_PROVIDERS]
 })
 @Routes([
-  {path: '/', component: HomeComponent},
-  {path: '/about', component: AboutComponent}
+  {path: '/ask-me-anything', component: HomeComponent},
+  {path: '/ask-me-anything/about', component: AboutComponent}
 ])
 export class AskMeAnythingAppComponent {
   constructor(private router:Router) {
